3b36125821faf3d4b43272cf8f9c4e5850e93cae
David Hasenjaeger <davehasen@gmail.com>
2015-02-18 14:02:07 -0800

    populates the squares array

---
+++ sudoku.js
@@ -0,0 +1,25 @@
+var input = '158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413';
+var i;
+var square = {};
+var squares = [];
+
+for (i = 0; i < input.length; i++) {
+
+	// determine whether the square is known
+	if (input[i] === ' ') {
+		square.known = false;
+		square.possible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	} else {
+		square.known = true;
+		square.possible = [Number(input[i])];
+	}
+	
+	// determine what row, column, and box the square belongs to
+	square.row = Math.floor(i / 9);
+	square.column = i % 9;
+	square.box = Math.floor(square.row / 3) * 3 + Math.floor(square.column / 3);
+	
+	// put the square into the array of squares
+	squares.push(square);
+	
+}
