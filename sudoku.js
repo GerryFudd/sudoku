bb8aee3bda6f1e736ad810928a7b9c505552bf56
David Hasenjaeger <davehasen@gmail.com>
2015-02-19 10:45:28 -0800

    comments added

--- sudoku.js
+++ sudoku.js
@@ -6,7 +6,7 @@ var check = 0;
 
 console.log('=======================================================================');
 
-
+// Make the initial state of the board
 function populateSquares ( i ) {
 
 	var square = {};
@@ -37,9 +37,11 @@ function populateSquares ( i ) {
 
 populateSquares(0);
 
+// Start solving
 function solve () {
 
 	var prevCheck = check;
+	// call one function for each technique
 	squares.map(narrowPossibilities);
 	squares.map(doubleCheck);
 	
@@ -57,6 +59,8 @@ function solve () {
 
 solve();
 
+// this function "pencils in" the possible values for each square.  It also sets a value
+// if there is only one possibility.
 function narrowPossibilities (square) {
 	if (!square.known) {
 	
@@ -91,6 +95,8 @@ function checker (depList, callback) {
 	callback(fn.intersect(limits[0], fn.intersect(limits[1], limits[2])));
 }
 
+// This function looks at the row, column, and box that each square belongs to
+// If a square is the only one that can hold a given value, assign that value to the box
 function doubleCheck (square) {
 	if (!square.known) {
 		doubleChecker(square, function (limit) {
@@ -106,9 +112,14 @@ function doubleCheck (square) {
 	}
 }
 
+
 function doubleChecker (square, callback) {
 	var dependencies = ['row', 'column', 'box'];
 	var near = {};
+	
+	// this loop populates the near object with row, column, and box keys
+	// each key will point to an array that contains the possible values for the
+	// other squares in the given row, column, or box
 	dependencies.forEach( function (elem, index) {
 		near[elem] = squares.reduce ( function (prev, current) {
 			if (current.row !== square.row || current.column !== square.column) {
@@ -124,6 +135,11 @@ function doubleChecker (square, callback) {
 		}, []);
 	});
 	
+	// This function checks each possible value of the current square against the other
+	// squares in its row.  If a possible value belongs only to the present square,
+	// then that value is assigned to the square.  If none of the values are unique to the
+	// present square, move on to the other squares in the column.  Then do the same with
+	// the box.
 	function crossHatch ( index ) {
 		var result = square.possible;
 		square.possible.forEach( function (elem, i) {
@@ -135,6 +151,10 @@ function doubleChecker (square, callback) {
 					}
 				});
 				if (unique) {
+// 					console.log(square);
+// 					console.log('was compared with its ' + dependencies[index]);
+// 					console.log(near[dependencies[index]]);
+// 					console.log('It was determined that ' + elem + ' is the only possible value');
 					result = [elem];
 				}
 			}
